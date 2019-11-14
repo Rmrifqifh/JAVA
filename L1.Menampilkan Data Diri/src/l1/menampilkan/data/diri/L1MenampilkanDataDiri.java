1e6168fab11d4aaf772649cd5b16028c3eaa2a10
Rmrifqifh <rifqi.orok@gmail.com>
2019-11-14 16:33:40 +0700

    first commit

---
+++ L1.Menampilkan Data Diri/src/l1/menampilkan/data/diri/L1MenampilkanDataDiri.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l1.menampilkan.data.diri;
+
+/**
+ * NAMA  : R MMUHAMMAD RIFQI FEBRIAN HADIANA
+ * NIM   : A2.1900136
+ * Kelas : TI-1B
+ * 
+ * @author RMRIFQIFH
+ */
+public class L1MenampilkanDataDiri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Biodata" + "\n" + "-----------------" );
+        System.out.println("NAMA\t\t         : R Muhammad Rifqi Febrian Hadiana");
+        System.out.println("ALAMAT\t\t         : Cipada 28F ");
+        System.out.println("TEMPAT LAHIR\t\t : Sumedang ");
+        System.out.println("TANGGAL LAHIR\t\t : 17 Februari 2002");
+        System.out.println("--------------------------");
+        System.out.println("HOBI\t\t: Bermain Game");
+        System.out.println("MAKES\t\t: Burger");
+        System.out.println("MIKES\t\t: Coffee");
+    }
+    
+}
